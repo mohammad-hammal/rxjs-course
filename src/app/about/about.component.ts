0f6f60f3eef18ecc810969e0717b66791bb3dbf3
Mohammad Hammal <MohammadHammal10@gmail.com>
2021-07-31 17:07:23 +0300

    Update about.component.ts

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,26 +1,42 @@
-import {Component, OnInit, ViewEncapsulation} from '@angular/core';
-import {concat, fromEvent, interval, noop, observable, Observable, of, timer, merge} from 'rxjs';
-import {delayWhen, filter, map, take, timeout} from 'rxjs/operators';
-import {createHttpObservable} from '../common/util';
-
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import {
+  concat,
+  fromEvent,
+  interval,
+  noop,
+  observable,
+  Observable,
+  of,
+  timer,
+  merge,
+  Subject,
+  BehaviorSubject,
+} from "rxjs";
+import { delayWhen, filter, map, take, timeout } from "rxjs/operators";
+import { createHttpObservable } from "../common/util";
 
 @Component({
-    selector: 'about',
-    templateUrl: './about.component.html',
-    styleUrls: ['./about.component.css']
+  selector: "about",
+  templateUrl: "./about.component.html",
+  styleUrls: ["./about.component.css"],
 })
 export class AboutComponent implements OnInit {
+  ngOnInit() {
+    // const subject = new Subject();
+    const subject = new BehaviorSubject(0);
 
-    ngOnInit() {
+    const series$ = subject.asObservable();
 
+    series$.subscribe(console.log);
 
-    }
+    subject.next(1);
+    subject.next(2);
+    subject.next(3);
+    // subject.complete();
 
-
+    setTimeout(() => {
+      series$.subscribe((val) => console.log(`Late Sub ${val}`));
+      // subject.next(4);
+    }, 3000);
+  }
 }
-
-
-
-
-
-
