95d6d1a3c7dcaaefc271ee46c4a9da327dfe881f
Mohammad Hammal <MohammadHammal10@gmail.com>
2021-08-02 16:46:16 +0300

    Done

--- src/app/common/store.service.ts
+++ src/app/common/store.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { Observable, BehaviorSubject } from "rxjs";
-import { map, tap } from "rxjs/operators";
+import { fromPromise } from "rxjs/internal-compatibility";
+import { filter, map, tap } from "rxjs/operators";
 import { Course } from "../model/course";
 import { createHttpObservable } from "./util";
 
@@ -8,8 +9,8 @@ import { createHttpObservable } from "./util";
   providedIn: "root",
 })
 export class Store {
-  private subject = new BehaviorSubject([]);
-  courses$: Observable<Course[]> = this.subject.asObservable();
+  private coursesSubject = new BehaviorSubject([]);
+  courses$: Observable<Course[]> = this.coursesSubject.asObservable();
 
   init() {
     const http$ = createHttpObservable("/api/courses");
@@ -18,7 +19,7 @@ export class Store {
         tap(() => console.log("HTTP request executed")),
         map((res) => Object.values(res["payload"]))
       )
-      .subscribe((courses: any) => this.subject.next(courses));
+      .subscribe((courses: any) => this.coursesSubject.next(courses));
   }
 
   selectBeginnersCourses() {
@@ -31,7 +32,37 @@ export class Store {
 
   filterByCategory(category: string) {
     return this.courses$.pipe(
-      map((courses) => courses.filter((course) => course.category == category))
+      map((courses) => courses.filter((course) => course.category === category))
+    );
+  }
+
+  saveCourse(courseId: number, changes) {
+    const courses = this.coursesSubject.getValue();
+    const courseIndex = courses.findIndex((course) => course.id === courseId);
+
+    const newCourses = [...courses];
+    newCourses[courseIndex] = {
+      ...courses[courseIndex],
+      ...changes,
+    };
+
+    this.coursesSubject.next(newCourses);
+
+    return fromPromise(
+      fetch(`/api/courses/${courseId}`, {
+        method: "PUT",
+        body: JSON.stringify(changes),
+        headers: {
+          "content-type": "application/json",
+        },
+      })
+    );
+  }
+
+  selectCourseById(courseId: number) {
+    return this.courses$.pipe(
+      map((courses) => courses.find((course) => course.id === courseId)),
+      filter((course) => !!course)
     );
   }
 }
